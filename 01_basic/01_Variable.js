acdb9949fbc3db7f68c020d5ca5656b164cb0536
Aboobakkar irshad i <irshadshanz231@gmail.com>
2024-03-09 08:10:28 +0000

    datatypes and sortinf correction

---
+++ 01_basic/01_Variable.js
@@ -0,0 +1,20 @@
+const accountId = 14524511;
+let accountEmail = "freak@google.com";
+var accountPassword = "1235614";
+accountCity = "delhi"; // is it possible to use but its Prohibited to use
+let accountState;
+
+
+//accountId = 26555455;   // is not allowed to change the const variable
+
+
+/*
+here var is not used anymore beacuse of block scope and functional scope
+*/
+
+accountEmail = "hamed@gamil.com";
+accountPassword = "69564979";
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+console.log(3
+    +
+    3); // dont do this // lacks Readbility
+
+//Types of datatypes
+/*  
+    Primitive data type.    
+
+    number = 2to power 53
+    bigint 
+    string = "something"
+    boolean = true/false
+    null = standalone value
+    undefined = value not assigned yet
+    symbol = unqiue 
+*/
+
+/*
+    non-Primitive data type or reference data type
+
+    obejct
+    array
+    function
+*/
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // obejct
